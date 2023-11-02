c71986cb32abe1231a1c936569f8eb78ae17efce
jcastano97 <jcastano1997@gmail.com>
2023-11-01 21:47:00 -0500

    add: cors *

--- src/api.ts
+++ src/api.ts
@@ -4,10 +4,10 @@ import cors from 'cors';
 import healthCheck from '@components/healthcheck/healthCheck.router';
 import user from '@components/account/user/user.router';
 
-const frontUrl = process.env.APP_FRONT_URL || 'http://localhost:3000';
+// const frontUrl = process.env.APP_FRONT_URL || 'http://localhost:3000';
 
 const router: Router = Router();
-router.use('/', cors({ origin: frontUrl }));
+router.use('/', cors({ origin: '*' }));
 router.use(healthCheck);
 router.use(user);
 
